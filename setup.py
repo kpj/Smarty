d5dda629092e97a1715f9be759e19f5751d40aac
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-08-14 11:13:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
 	name='smarty',
-	version='0.3.3',
+	version='0.3.4',
 	description='Smart playlist creation for mpd',
 	long_description=readme(),
 	url='https://github.com/kpj/Smarty',
